24f3bd219c8c692b0dbd6646ab066561df2d7151
Rajiv Giri <rajivgiri513@gmail.com>
2022-01-29 01:20:54 -0500

    Third Commit[Running Multiple threads at the same time.]

---
+++ src/com/rajivgiri/runnables/ApplicationThread.java
@@ -0,0 +1,30 @@
+package com.rajivgiri.runnables;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+
+public class ApplicationThread extends Thread {
+	
+	@Override
+	
+	public void run() {
+		try(BufferedReader read = new BufferedReader(new FileReader("C:\\Users\\rgiri\\OneDrive\\Desktop\\Bootcamp\\Power punch 60 second elevator pitch and penut butter jelly sandwich.txt"))){
+			String line = null;
+			
+			while((line=read.readLine())!=null){
+				System.out.println(Thread.currentThread().getName()+"Reading your resume" + line);
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+}
---
+++ src/com/rajivgiri/tests/TestThreads.java
@@ -0,0 +1,20 @@
+package com.rajivgiri.tests;
+
+import com.rajivgiri.runnables.ApplicationThread;
+
+public class TestThreads {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		ApplicationThread thread1 = new ApplicationThread();
+		ApplicationThread thread2 = new ApplicationThread();
+		ApplicationThread thread3 = new ApplicationThread();
+		
+		thread1.start();
+		thread2.start();
+		thread3.start();
+		
+	}
+
+}
