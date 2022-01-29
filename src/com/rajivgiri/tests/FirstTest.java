43df87dc082a73b25ca691f6a93da212593f6bea
Rajiv Giri <rajivgiri513@gmail.com>
2022-01-29 00:34:34 -0500

    First Commit

---
+++ src/com/rajivgiri/tests/FirstTest.java
@@ -0,0 +1,10 @@
+package com.rajivgiri.tests;
+
+public class FirstTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Yay, so exicited to learn Java");
+	}
+
+}
